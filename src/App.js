9a94a91b832dee300266dce0f02445742ea5002d
blessed1797 <blessedlyjummy@gmail.com>
2022-11-22 12:15:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import line from './pattern-divider-desktop.svg'
+import button from './icon-dice.svg'
+import axios from 'axios';
+import { useState } from 'react'
+
+
 
 function App() {
+
+  const [advice, setAdvice] = useState({})
+  const getAdvice = (id) => {
+    id ? axios.get(`https://api.adviceslip.com/advice/${id}`).then(res => setAdvice(res.data.slip)).catch(err => console.log(err)) :
+      axios.get("https://api.adviceslip.com/advice").then(res => setAdvice(res.data.slip)).catch(err => console.log(err))
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='app2'>
+        <h6>Advice #{advice.id ? advice.id : "00"}</h6>
+        <h4>{advice.advice ? advice.advice : "click the button below to get an advice"}</h4>
+      </div>
+      <div><img className='desktop' src={line} alt="" /></div>
+      <button onClick={() => getAdvice()}><img className='desktop2' src={button} alt="" /></button>
+      {/* <button onClick={() => getAdvice(30)}>30</button>
+      <button onClick={() => getAdvice(50)}>50</button>
+      <button onClick={() => getAdvice(60)}>60</button> */}
+
+
     </div>
   );
 }
